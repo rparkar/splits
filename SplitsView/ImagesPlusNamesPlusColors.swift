249cfd5047e29e5ccbe82cecba9ca7399578f17f
Rehan Parkar <parkarcamp@gmail.com>
2018-03-18 17:43:57 -0500

    created datamodel Dummy and some initial code clean up

---
+++ SplitsView/ImagesPlusNamesPlusColors.swift
@@ -0,0 +1,29 @@
+//
+//  ImagesPlusNamesPlusColors.swift
+//  SplitsView
+//
+//  Created by Rehan Parkar on 2018-03-18.
+//  Copyright © 2018 Rehan Parkar. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let camera = UIImage(named: "1")!
+let city = UIImage(named: "2")!
+let buffalo = UIImage(named: "3")!
+let bouqet = UIImage(named: "4")!
+let stand = UIImage(named: "5")!
+let urban = UIImage(named: "6")!
+
+//data model dummy
+
+//names disolay in tableview
+var namesArray = ["CAMERA", "CITY", "ANIMALS", "FLOWER", "STANDS", "URBAN"]
+
+//array of images
+let imageArray = [camera, city, buffalo, bouqet, stand, urban]
+
+//colors array for the table view
+let colorsArray = [#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)]
+
---
+++ SplitsView/MasterViewController.swift
@@ -0,0 +1,56 @@
+//
+//  MasterViewController.swift
+//  SplitsView
+//
+//  Created by Rehan Parkar on 2018-03-18.
+//  Copyright © 2018 Rehan Parkar. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+        
+    }
+
+
+
+    // MARK: - Segues
+
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if segue.identifier == "showDetail" {
+//            if let indexPath = tableView.indexPathForSelectedRow {
+//                let object = objects[indexPath.row] as! NSDate
+//                let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
+//                controller.detailItem = object
+//                controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+//                controller.navigationItem.leftItemsSupplementBackButton = true
+//            }
+//        }
+//    }
+
+    // MARK: - Table View
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return imageArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        cell.textLabel!.text = namesArray[indexPath.row]
+        cell.backgroundColor = colorsArray[indexPath.row]
+        return cell
+    }
+
+
+}
+
